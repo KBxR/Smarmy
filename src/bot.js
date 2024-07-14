447753c9924d8a0275c198a1186b8b306568544e
Connor McFarland <KBxR@users.noreply.github.com>
2024-07-14 01:14:32 +0100

    renamed subcommand folder to match main commands name

--- src/bot.js
+++ src/bot.js
@@ -1,7 +1,7 @@
 require('module-alias/register');
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates] });
 const config = require('@config/config');
 
--- src/commands/lastfm/lastfm.js
+++ src/commands/lastfm/lastfm.js
@@ -2,14 +2,14 @@ const { SlashCommandBuilder } = require('discord.js');
 const fs = require('fs');
 const path = require('path');
 
-const commandFiles = fs.readdirSync(path.join(__dirname, 'subcommands')).filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync(path.join(__dirname, 'lastfm')).filter(file => file.endsWith('.js'));
 
 const command = new SlashCommandBuilder()
     .setName('lastfm')
     .setDescription('LastFM Options');
 
 for (const file of commandFiles) {
-    const commandData = require(`./subcommands/${file}`);
+    const commandData = require(`./lastfm/${file}`);
     command.addSubcommand(() => commandData.data);
 }
 
@@ -17,7 +17,7 @@ module.exports = {
     data: command,
     async execute(interaction) {
         const subcommand = interaction.options.getSubcommand();
-        const commandFile = require(`./subcommands/${subcommand}.js`);
+        const commandFile = require(`./lastfm/${subcommand}.js`);
         await commandFile.execute(interaction);
     }
 };
