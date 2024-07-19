0243897b926f79321b54c6eae6e1435efb0ec4da
Connor McFarland <KBxR@users.noreply.github.com>
2024-07-19 04:21:13 +0100

    added more variable support to methods

--- src/api/lastFm.js
+++ src/api/lastFm.js
@@ -8,47 +8,66 @@ async function makeLastFmApiCall(method, params) {
     return res.data;
 }
 
-async function getWeeklyTopArtists(username) {
-    const data = await makeLastFmApiCall('user.getweeklyartistchart', `&user=${username}`);
-    return data.weeklyartistchart.artist;
-}
+// USER METHODS
 
-async function getTopArtists(username) {
-    const data = await makeLastFmApiCall('user.gettopartists', `&user=${username}`);
-    return data.topartists.artist;
-}
-
-async function getWeeklyScrobbles(username) {
-    const data = await makeLastFmApiCall('user.getweeklytrackchart', `&user=${username}`);
-    return data.weeklytrackchart.track;
-}
-
+// Gets the user info of a user
 async function getLastFmUser(username) {
     const data = await makeLastFmApiCall('user.getinfo', `&user=${username}`);
     return data.user;
 }
 
+// Gets the weekly scrobbles of a user
+async function getWeeklyScrobbles(username) {
+    const data = await makeLastFmApiCall('user.getweeklytrackchart', `&user=${username}`);
+    return data.weeklytrackchart.track;
+}
+
+// TRACKS METHODS
+
+// Gets the recent tracks of a user
 async function getRecentTracks(username, length) {
     length = length || 1;
     const data = await makeLastFmApiCall('user.getRecentTracks', `&user=${username}&nowplaying=true&limit=${length}`);
     return data.recenttracks.track;
 }
 
+// Gets the top tracks of a user
+async function getTopTracks(username, length, period) {
+    length = length || 10;
+    period = period || '7day';
+    const data = await makeLastFmApiCall('user.gettoptracks', `&user=${username}&limit=${length}&period=${period}`);
+    return data.toptracks.track;
+}
+
+// ARTIST METHODS
+
+// Gets the weekly top artists of a user
+async function getWeeklyTopArtists(username) {
+    const data = await makeLastFmApiCall('user.getweeklyartistchart', `&user=${username}`);
+    return data.weeklyartistchart.artist;
+}
+
+// Gets the top artists of a user
+async function getTopArtists(username) {
+    const data = await makeLastFmApiCall('user.gettopartists', `&user=${username}`);
+    return data.topartists.artist;
+}
+
+// Gets the info of an artist
 async function getArtistInfo(artist) {
     const data = await makeLastFmApiCall('artist.getinfo', `&artist=${artist}`);
     return data.artist;
 }
 
+// Gets the info of an artist with a username
 async function getArtistInfoWUsername(artist, username) {
     const data = await makeLastFmApiCall('artist.getinfo', `&artist=${artist}&username=${username}`);
     return data.artist;
 }
 
-async function getTopTracks(username) {
-    const data = await makeLastFmApiCall('user.gettoptracks', `&user=${username}&limit=10`);
-    return data.toptracks.track;
-}
+// ALBUM METHODS
 
+// Gets the top albums of a user
 async function getTopAlbums(username, length) {
     length = length || 10;
     const data = await makeLastFmApiCall('user.gettopalbums', `&user=${username}&limit=${length}`);
