d9dd48728008c5539283810d81f42669a32f3ac3
Connor McFarland <KBxR@users.noreply.github.com>
2024-07-19 04:22:11 +0100

    no longer needed

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,12 +1,10 @@
 const botInfoUtil = require('@utils/botInfoUtil');
 const DBHandler = require('@utils/DBHandler');
-//const pixelateImage = require('../commands/lastfm/utils/pixelateImage');
 const randomColor = require('@utils/randomColor');
 const resolveUsername = require('../commands/lastfm/utils/usernameResolver');
 
 module.exports = {
     DBHandler,
-    //pixelateImage,
     botInfoUtil,
     randomColor,
     resolveUsername
