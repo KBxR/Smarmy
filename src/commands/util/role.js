8af5846b7908d0f62f72192567a9c828d171dbc1
Connor McFarland <KBxR@users.noreply.github.com>
2024-08-08 10:49:49 +0100

    added permission for role commands

--- src/commands/util/role.js
+++ src/commands/util/role.js
@@ -1,6 +1,7 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, PermissionsBitField } = require('discord.js');
 const fs = require('fs');
 const path = require('path');
+const { hasPermission } = require('@utils/permissions');
 
 const commandFiles = fs.readdirSync(path.join(__dirname, 'role')).filter(file => file.endsWith('.js'));
 
@@ -16,6 +17,22 @@ for (const file of commandFiles) {
 module.exports = {
     data: command,
     async execute(interaction) {
+
+        async function hasRolesPermission(serverId, userId, member) {
+            const hasCustomPermission = await hasPermission(serverId, userId, 'roles');
+            const hasManageRolesPermission = member.permissions.has(PermissionsBitField.Flags.ManageRoles);
+        
+            return hasCustomPermission || hasManageRolesPermission;
+        }
+
+        const serverId = interaction.guild.id;
+        const userId = interaction.user.id;
+        const member = interaction.member;
+
+        if (!await hasRolesPermission(serverId, userId, member)) {
+            return interaction.reply({ content: 'You do not have permission to use this command.', ephemeral: true });
+        }
+
         const subcommand = interaction.options.getSubcommand();
         const commandFile = require(`./role/${subcommand}.js`);
         await commandFile.execute(interaction);
