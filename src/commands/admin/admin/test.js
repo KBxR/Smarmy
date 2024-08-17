763a6be21645a8fc9a78742653f73ca4c6369660
Connor McFarland <KBxR@users.noreply.github.com>
2024-08-17 20:32:41 +0100

    channel

--- src/commands/admin/admin/test.js
+++ src/commands/admin/admin/test.js
@@ -3,9 +3,15 @@ const { sendWOTD } = require('@events/cheadlewotd');
 
 module.exports.data = new SlashCommandSubcommandBuilder()
     .setName('test')
-    .setDescription('Test command.');
+    .setDescription('Test command.')
+    .addStringOption(option =>
+        option.setName('channel')
+            .setDescription('The channel ID to send the test message to.')
+            .setRequired(true));
+            
 
 module.exports.execute = async function handleTest(interaction) {
-    const client = interaction.client; // Get the client from the interaction object
-    await sendWOTD(client, '1076196921682702356');
+    const client = interaction.client;
+    const channelId = interaction.options.getString('channel');
+    await sendWOTD(client, channelId);
 };
